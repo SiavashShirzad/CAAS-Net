e1bc06b7adbdbefc9e7befe42354fedbb975103e
SiavashShirzad <siavashshirzadeh@gmail.com>
2022-08-27 22:44:33 +0430

    model builder, compile, fit, predict

--- datacleaning.py
+++ datacleaning.py
@@ -5,14 +5,18 @@ import matplotlib
 from tqdm import tqdm
 import nibabel as nib
 
+
+def vid_to_array(path):
+    vid = dicom.dcmread(path)
+    vid = vid.pixel_array
+
+
 class DataCleaning():
-    def __init__(self,data_path,save_path):
+    def __init__(self, data_path, save_path):
         self.data_path = data_path
         self.save_path = save_path
         self.affine = None
-    def vid_to_array(self,path):
-        vid = dicom.dcmread(path)
-        vid = vid.pixel_array
+
     def dicom_to_png(self):
         angio_list = os.listdir(self.data_path)
         for name in tqdm(angio_list):
@@ -20,14 +24,14 @@ class DataCleaning():
                 for image in os.listdir(self.data_path + "/" + name + "/SR 1"):
                     if image[0] == 'I':
                         i = 0
-                        for img in self.vid_to_array(self.data_path + "/" + name + "/SR 1" + "/" + image):
+                        for img in vid_to_array(self.data_path + "/" + name + "/SR 1" + "/" + image):
                             i = i + 1
                             matplotlib.image.imsave(self.save_path + name + "$" + image + "$" + str(i) + ".png", img,
                                                     cmap="gray")
             except:
                 for image in os.listdir(self.data_path + "/" + name + "/d0"):
                     if image[0] == 'S':
-                        for img in self.vid_to_array(self.data_path + "/" + name + "/SR 1" + "/" + image):
+                        for img in vid_to_array(self.data_path + "/" + name + "/SR 1" + "/" + image):
                             i = i + 1
                             matplotlib.image.imsave(self.save_path + name + "$" + image + "$" + str(i) + ".png", img,
                                                     cmap="gray")
@@ -35,6 +39,7 @@ class DataCleaning():
                 continue
 
     # recommended method for cleaning data 1. easier to anonymize (smaller header) 2. gives 3d arrays 3. can be used in rilcontour (great free annotation app) 4. preseves the pixel data
+
     def dicom_to_nifti(self):
         angio_list = os.listdir(self.data_path)
         for cd in tqdm(angio_list[9:]):
@@ -43,8 +48,9 @@ class DataCleaning():
                     for image in os.listdir(self.data_path + "/" + cd + '/' + name):
                         try:
                             if image[0] == 'S':
-                                nib.save(nib.Nifti1Image(self.vid_to_array(self.data_path + "/" + cd + '/' + name + "/" + image),
-                                                         self.affine),
-                                         self.save_path + cd + '$' + name + "$" + image + '.nii.gz')
+                                nib.save(nib.Nifti1Image(
+                                    vid_to_array(self.data_path + "/" + cd + '/' + name + "/" + image),
+                                    self.affine),
+                                    self.save_path + cd + '$' + name + "$" + image + '.nii.gz')
                         except:
                             continue
---
+++ model.py
@@ -0,0 +1,29 @@
+from models import BuildDeepLabV3, BuildEfficientB0Unet
+
+
+class ModelBuilder:
+
+    def __init__(self, image_size, number_classes, model_name):
+        self.image_size = image_size
+        self.number_classes = number_classes
+        self.model_name = model_name
+
+        if self.model_name == "deeplab":
+            model_builder = BuildDeepLabV3()
+            self.model = model_builder(self.image_size, self.number_classes)
+
+        if self.model_name == "efficientb0":
+            model_builder = BuildEfficientB0Unet()
+            self.model = model_builder(self.image_size, self.number_classes)
+
+    def summary(self):
+        return self.model.summary()
+
+    def compile(self, optimizer, loss, metrics):
+        self.model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+
+    def fit(self, dataset, validation_data, epochs, callbacks):
+        self.model.fit(dataset, validation_data=validation_data, epochs=epochs, callbacks=callbacks)
+
+    def predict(self, data):
+        return self.model.predict(data)
