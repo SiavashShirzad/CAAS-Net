789675ba2bc01709916fe9953c4d865457d23a22
SiavashShirzad <siavashshirzadeh@gmail.com>
2022-09-08 23:03:21 +0430

    visualization

---
+++ visualization.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+class visualizer:
+
+    def __init__(self, main_image, main_mask, predicted_mask, predicted_image):
+        self.main_image = main_image
+        self.main_mask = main_mask
+        self.predicted_mask = predicted_mask
+        self.predicted_image = predicted_image
+
+    def visualize(self):
+
+        pass
