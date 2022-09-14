ecee9f1cdc0cada58c5e4768bb9f61e03ebd04d0
SiavashShirzad <siavashshirzadeh@gmail.com>
2022-09-14 22:42:15 +0430

    DenseNet UUNet

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from model import ModelBuilder
 import tensorflow as tf
 
-model = ModelBuilder(512, 5, 'DenseNet')
+model = ModelBuilder(512, 5, 'DenseNetUUnet')
 model.compile(
     optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-    metrics=["accuracy"])
+    loss={'multi': tf.keras.losses.SparseCategoricalCrossentropy(),
+          'single': tf.keras.losses.BinaryCrossentropy()},
+    metrics={'multi': ['accuracy'],
+             'single': ['accuracy']})
 print(model.summary())
